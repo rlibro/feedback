1205a44873fe613c2a290bd0d73793b5e78b420e
ByungDae Sohn <miconblog@gmail.com>
2016-01-05 16:49:13 -0600

    페이스북 API를 이용해 사용자의 국가 정보를 얻어온다.
    ES2015의 Template Strings 문법을 적용함.

--- server/routes/facebook/index.js
+++ server/routes/facebook/index.js
@@ -1,20 +1,25 @@
-/**
- * 2015 페이지 라우팅
- */
 'use strict';
 
-var express = require('express');
-var router = express.Router();
-var config = require('../../../config/environment');
-var request = require('request');
+const express = require('express');
+const router = express.Router();
+const config = require('../../../config/environment');
+const request = require('request');
+const fb_scope = 'user_location,user_friends,email';
+const fb_api_version = 'v2.5';
+
 
 router.get('/login?', function(req, res) {
 
-  var redirect_url = config.facebook.hostname + '/facebook/result&scope=publish_actions,email';
+  const redirect = req.query.redirect;
+  const redirect_url = `${config.facebook.hostname}/facebook/result&scope=${fb_scope}`;
+
+  if( redirect ) {
+    req.session.redirect = redirect;
+  }
 
   res.writeHead(301, {
     'Cache-Control': 'no-cache',
-    'Location': 'https://www.facebook.com/dialog/oauth?client_id=' + config.facebook.api_key + '&redirect_uri=' + redirect_url
+    'Location': `https://www.facebook.com/dialog/oauth?client_id=${config.facebook.api_key}&redirect_uri=${redirect_url}`
   });
   res.end();
 });
@@ -22,29 +27,60 @@ router.get('/login?', function(req, res) {
 router.get('/result?', function(req, res) {
 
   if (req.query.code) {
-    var redirect = req.session.lastUrl || 'http://localhost:3200/';
-    var url = 'https://graph.facebook.com/oauth/access_token?scope=email&client_id=' + config.facebook.api_key + '&redirect_uri=' + encodeURIComponent(config.facebook.hostname + req.originalUrl) + '&client_secret=' + encodeURIComponent(config.facebook.secret) + '&code=' + encodeURIComponent(req.query.code);
+    const redirect = req.session.redirect || config.facebook.hostname;
+    const url = `https://graph.facebook.com/oauth/access_token?scope=${fb_scope}&client_id=${config.facebook.api_key}&redirect_uri=${encodeURIComponent(config.facebook.hostname + req.originalUrl)}&client_secret=${config.facebook.secret}&code=${encodeURIComponent(req.query.code)}`;
 
-    request(url, function(err, resp, body) {
-      var token = (require('querystring').parse(body).access_token);
+    request({ 
+      uri: url,
+      timeout: 100000
+    }, function(err, resp, body) {
+      
+      if( err ){
+        console.err('[module][facebook][error]', err);
+        res.send("<!DOCTYPE html><script>window.opener.location.reload(); window.close()</script>");
+        return;
+      }
+
+      const token = (require('querystring').parse(body).access_token);
 
       if (token) {
-        request('https://graph.facebook.com/v2.3/me?fields=id,name,email,picture{url}&access_token=' + token, 
+        
+        request(`https://graph.facebook.com/${fb_api_version}/me?fields=id,name,email,location,picture{url}&access_token=${token}`, 
           function(err, resp, body) {
 
-          var data = JSON.parse(body);
-          
+          if( err && err.code === 'ETIMEDOUT') { 
+            res.send("<!DOCTYPE html><script>window.opener.location.reload(); window.close()</script>");
+            return;
+          }
+
+          const data = JSON.parse(body);
           req.session.user = data;
           req.session.user.provider = 'facebook';
           req.session.user.ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
           req.session.save();
 
-          res.send("<!DOCTYPE html><script>window.opener.location.href = '" + redirect + "'; window.close()</script>");
+          request(`https://graph.facebook.com/${fb_api_version}/${data.location.id}?fields=location&access_token=${token}`, function(err, resp, body){
+
+            const data = JSON.parse(body);
+            req.session.user.location = data.location;
+            req.session.save();
+
+            res.send(`<!DOCTYPE html><script>window.opener.location.href='${redirect}';window.close()</script>`);
+
+
+          });
+
+          
         
         });
-      } else { // something error happened: we just close...
+
+      } else {
+        
+        console.err('[module][facebook][exception] - invalid token')
         res.send("<!DOCTYPE html><script>window.opener.location.reload(); window.close()</script>");
+      
       }
+
     });
   } 
 
