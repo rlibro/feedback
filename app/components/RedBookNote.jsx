25fc9700887acec9e89ed5e0cca66fe3fe5a3384
ByungDae Sohn <miconblog@gmail.com>
2016-01-21 12:07:38 -0600

    노트에 있는 지도 마커 클릭시 스크롤 상단으로 올림

--- app/components/RedBookNote.jsx
+++ app/components/RedBookNote.jsx
@@ -178,8 +178,8 @@ export default class RedBookNote extends Component {
   handleContentLink = (e) => {
 
     var link = e.target.href.split('notes')[1];
-
     this.props.onPushState('/notes' + link);
+    window.scrollTo(0,0);
     e.preventDefault();
 
   };
