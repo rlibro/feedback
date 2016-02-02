c1547be3d760a577baf405fed9b1fabf5a27a011
ByungDae Sohn <miconblog@gmail.com>
2016-02-01 23:56:04 -0500

    # 에디터 문법 보강
    마크다운 풀 지원 및 스타일 조정

--- app/libs/markdown.js
+++ app/libs/markdown.js
@@ -1,22 +1,28 @@
+var marked = require('marked');
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: true,
+  sanitize: true,
+  smartLists: true,
+  smartypants: true
+});
+
+
 function makeMarkerLink(text, id) {
   return text
     .replace(/\[([^\[\]]*)\]\[(\d+)\]/g, `<a target="_blank" href="/notes/${id}/places/$2"><i class="fa icon-pin"></i>$1</a>`)
-    .replace(/(.*)\n\n(.*)/g, '<p>$1</p><br/><p>$2</p>')
-    .replace(/(.*)\n(.*)/g, '<p>$1</p><p>$2</p>')
-    .replace(/\s\s/g, '<span></span>');
-}
-
-function makeLink(text) {
-
-  return text
-    .replace(/\[([^\[\]]*)\]\((https?:\/\/[^\(\)]*)\)/g, `<a target="_blank" href="$2"><i class="fa icon-pin"></i>$1</a>`)
+    //.replace(/(.*)\n\n(.*)/g, '<p>$1</p><br/><p>$2</p>')
+    //.replace(/(.*)\n(.*)/g, '<p>$1</p><p>$2</p>')
+    //.replace(/\s\s/g, '<span></span>');
 }
 
 export function render (text, noteId) {
 
+  text = marked(text);
   text = makeMarkerLink(text, noteId);
-  text = makeLink(text);
-
-
+    
   return text;
 }
